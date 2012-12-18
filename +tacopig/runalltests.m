7b19b95504afb4ec0c5e6a70d9f48d7655fa5da1
Alistair Reid <alistair.reid@nicta.com.au>
2012-12-18 17:35:58 +1100

    Updating links for directory re-structure

--- +tacopig/runalltests.m
+++ +tacopig/runalltests.m
@@ -1,2 +1,8 @@
-addpath('matlab-xunit-doctest/xunit')
-runtests tacopigtest
+function runalltests(varargin)
+    % make sure we change to the right directory...
+    mfilepath = mfilename('fullpath');
+    mfilepath = mfilepath(1:end-length(mfilename())-length('+TacoPig/'));
+    testdir = [mfilepath,'matlab-xunit-doctest/xunit']
+    addpath(testdir)
+    runtests('tacopig.tests')
+    return;
--- +tacopig/runalltestsXML.m
+++ +tacopig/runalltestsXML.m
@@ -1,2 +1,8 @@
-addpath('matlab-xunit-doctest/xunit')
-runtests tacopigtest -xmlfile testreport.xml
+function runalltestsXML(varargin)
+    % make sure we change to the right directory...
+    mfilepath = mfilename('fullpath');
+    mfilepath = mfilepath(1:end-length(mfilename())-length('+TacoPig/'));
+    testdir = [mfilepath,'matlab-xunit-doctest/xunit']
+    addpath(testdir)
+    runtests('tacopig.tests', '-xmlfile', 'testreport.xml')
+    return;
